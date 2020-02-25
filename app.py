5059803214e0fccfaaab5f766e4c08efa02ca18c
Rucha Bapat <bapatrucha@gmail.com>
2020-02-24 21:24:53 -0800

    moved init_app to run.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
@@ -14,12 +13,6 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'Bapat'
 api = Api(app)
-
-db.init_app(app)
-
-@app.before_first_request
-def createTables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity) # for authentication of user
 
@@ -30,4 +23,6 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
     app.run(port = 5000, debug=True)
